a15aecc5ab7f1231fdbccc249b2fd0d1f619908d
Scott Davis <scottdavisxx@gmail.com>
2026-02-28 16:17:54 -0700

    more progress on mobile nav

--- frontend/app/components/Navigation.tsx
+++ frontend/app/components/Navigation.tsx
@@ -54,29 +54,28 @@ export default function Navigation({
   }, []);
 
   return (
-    <div>
-      <nav
-        className={`px-2 py-4 w-full flex uppercase just fixed top-0 left-0 right-0 z-50 font-bold transition-colors duration-600 bg-dark-blue
+    <nav
+      className={`px-6 py-4 w-screen flex uppercase just fixed top-0 left-0 right-0 z-50 font-bold transition-colors duration-600 bg-dark-blue
         md:px-11 md:py-6 md:justify-between md:items-center md:bg-transparent
         ${scrolled ? "bg-dark-blue" : ""} ${block?.color === LogoColor.light ? "text-white ho" : "text-dark-blue"}`}
-      >
-        <div className="flex flex-col justify-between w-full items-start gap-6
-      md:flex-row md:container md:items-center md:gap-0">
-          <Link href="/">
-            <OrlsLogo
-              color={block?.color && block.color === 'white' ? LogoColor.light : LogoColor.dark}
-              className="w-9/12 md:w-auto"
-            />
-          </Link>
-          <div className="flex flex-col justify-between gap-8 pl-6
-        md:flex-row md:items-center md:pl-0">
-            {navigationItems.map((item) => (
-              <Link key={item.href} href={item.href} className={`${block?.color === LogoColor.light ? "hover:underline" : "hover:text-white"}`}>{item.label}</Link>
-            ))}
-          </div>
-        </div >
-        {/* <Bars3Icon onClick={handleToggle} className="w-7 h-7 absolute right-4 top-4 md:hidden" /> */}
-      </nav>
-    </div>
+    >
+      <div className={`flex flex-col justify-between w-full items-start
+      md:flex-row md:container md:items-center md:gap-0 ${isOpen ? "gap-6" : ""}`}>
+        <Link href="/">
+          <OrlsLogo
+            color={block?.color && block.color === 'white' ? LogoColor.light : LogoColor.dark}
+            className="w-9/12 md:w-auto"
+          />
+        </Link>
+        <div className={`flex flex-col justify-between gap-8 
+        md:flex-row md:items-center md:pl-0
+        ${isOpen ? "h-full" : "h-0 hidden"}`}>
+          {navigationItems.map((item) => (
+            <Link key={item.href} href={item.href} className={`${block?.color === LogoColor.light ? "hover:underline" : "hover:text-white"}`}>{item.label}</Link>
+          ))}
+        </div>
+      </div >
+      <Bars3Icon onClick={handleToggle} className="w-8 h-8 absolute right-5 top-5 md:hidden" />
+    </nav>
   );
 }
