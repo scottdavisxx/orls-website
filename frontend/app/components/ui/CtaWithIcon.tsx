50a1c88383edcf6eabb3cd68cbabbc19076c5026
Scott Davis <scottdavisxx@gmail.com>
2026-02-28 14:58:09 -0700

    make orls icon turn yellow on hover

--- frontend/app/components/ui/CtaWithIcon.tsx
+++ frontend/app/components/ui/CtaWithIcon.tsx
@@ -9,9 +9,9 @@ interface CtaWithIconProps {
 
 export default function CtaWithIcon({ href, buttonText, newTab }: CtaWithIconProps) {
   return (
-    <Link target={newTab ? "_blank" : "_self"} href={href} className="flex gap-4 items-center text-dark-blue hover:text-white w-fit uppercase font-bold z-10 transition-all duration-300">
+    <Link target={newTab ? "_blank" : "_self"} href={href} className="flex gap-4 items-center text-dark-blue hover:text-white w-fit uppercase font-bold z-10 transition-all duration-300 group">
       <span className="text-2xl px-24 py-4 bg-white hover:bg-dark-blue transition-all duration-300 rounded-sm">{buttonText}</span>
-      <OrlsIcon />
+      <OrlsIcon useCurrentColor className="text-white group-hover:text-yellow transition-colors duration-300" />
     </Link>
   );
 }
