5719cbddf5d0fc9021f69a633d487a8b17fdefc0
Scott Davis <scottdavisxx@gmail.com>
2026-02-28 19:46:12 -0700

    Refactor Footer component for improved mobile layout and responsiveness

--- frontend/app/components/Footer.tsx
+++ frontend/app/components/Footer.tsx
@@ -6,8 +6,8 @@ export default function Footer() {
     <footer className="flex flex-col items-center justify-between gap-6 bg-dark-blue text-white py-4 px-6 mb-64 pb-10
     md:flex-row md:px-24 md:mb-0 md:pb-0">
       <Image src="/logo-2.png" alt="Logo" width={424} height={239} />
-      <div className="flex flex-col justify-between gap-8 w-8/12
-       md:flex-row">
+      <div className="flex flex-col justify-between gap-8
+       md:flex-row md:w-8/12">
         <div>
           <h3 className="text-2xl font-bold">Contact Us</h3>
           <p>Phone: <a href="tel:217-368-1465">217-368-1465</a></p>
