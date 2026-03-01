733b5633b42a96ba8bf5f819d134401cffc46cb1
Scott Davis <scottdavisxx@gmail.com>
2026-02-28 21:47:57 -0700

    omg im going to sleep

--- frontend/app/components/ThreeColCtas.tsx
+++ frontend/app/components/ThreeColCtas.tsx
@@ -2,24 +2,27 @@ import Link from "next/link";
 
 export default function ThreeColCtas() {
   return (
-    <div className="bg-white flex">
-      <div className="flex flex-col items-stretch w-full text-center
+    <div className="bg-white">
+      <div className="flex
+      md:container">
+        <div className="flex flex-col items-stretch w-full text-center
       md:flex-row">
-        <Link href="#" className="flex flex-col gap-2 items-center justify-center border border-black py-4 px-12
+          <Link href="#" className="flex flex-col gap-2 items-center justify-center border border-black py-4 px-12
         md:1/3 md:py-16">
-          <h3 className="text-medium-blue text-3xl">Inquire</h3>
-          <p className="text-xl">Request more information about OR Dallas</p>
-        </Link>
-        <Link href="#" className="flex flex-col gap-2 items-center justify-center border border-black py-4 px-12
+            <h3 className="text-medium-blue text-3xl">Inquire</h3>
+            <p className="text-xl">Request more information about OR Dallas</p>
+          </Link>
+          <Link href="#" className="flex flex-col gap-2 items-center justify-center border border-black py-4 px-12
         md:1/3 md:py-16">
-          <h3 className="text-medium-blue text-3xl">Explore</h3>
-          <p className="text-xl">Attend a visit day, come to the open house, or take a campus tour</p>
-        </Link>
-        <Link href="#" className="flex flex-col gap-2 items-center justify-center border border-black py-4 px-12
+            <h3 className="text-medium-blue text-3xl">Explore</h3>
+            <p className="text-xl">Attend a visit day, come to the open house, or take a campus tour</p>
+          </Link>
+          <Link href="#" className="flex flex-col gap-2 items-center justify-center border border-black py-4 px-12
         md:1/3 md:py-16">
-          <h3 className="text-medium-blue text-3xl">Apply</h3>
-          <p className="text-xl">Begin or complete an application to attend OR Dallas</p>
-        </Link>
+            <h3 className="text-medium-blue text-3xl">Apply</h3>
+            <p className="text-xl">Begin or complete an application to attend OR Dallas</p>
+          </Link>
+        </div>
       </div>
     </div>
   )
