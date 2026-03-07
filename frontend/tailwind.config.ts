55b595e6ceddbb3eeb7c335238bc524e718dc602
sohalmani <67227540+sohalmani@users.noreply.github.com>
2026-03-07 09:24:12 +0530

    Update Tailwind configuration and global styles for improved layout and typography

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -4,9 +4,17 @@ import typography from '@tailwindcss/typography'
 export default {
   content: ['./app/**/*.{ts,tsx}', './sanity/**/*.{ts,tsx}'],
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+    },
     extend: {
+      boxShadow: {
+        layer: '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+      },
       fontFamily: {
-        sans: ['var(--font-inter)'],
+        sans: ['var(--font-albert-sans)'],
+        serif: ['var(--font-alegreya)'],
         mono: ['var(--font-ibm-plex-mono)'],
       },
     },
